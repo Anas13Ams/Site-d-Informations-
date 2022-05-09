a05316634a6a4adad56cdde5c89347e2814e59d4
Anas13Ams <91897163+Anas13Ams@users.noreply.github.com>
2022-05-09 17:08:31 +0200

    projetGeekIN

---
+++ geek-inn_front/src/components/EventList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card-list">
+    <span class="card-title">{{ event.title }} </span>
+    <img class="card-img" :src="event.galery_pictures" />
+    <span>{{ event.content }} </span>
+    <span class="price">{{ event.price }} €.TTC </span>
+    <span class="date">{{ event.date }}</span>
+    <router-link :to="routePath" class="button">Plus d'information</router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: "EventList",
+  props: { event: Object },
+  data() {
+    return { routePath: "/events/" + this.event.id };
+  },
+};
+</script>
+<style scoped>
+.card-img {
+  margin-left: 10px;
+}
+</style>
